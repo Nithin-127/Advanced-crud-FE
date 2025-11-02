842dc8ac713ec2534b644c6808616bf17e128807
Nithin-127 <nithinmanoj127@gmail.com>
2025-11-02 17:26:10 +0530

    fix api routes

--- src/Services/Axios.js
+++ src/Services/Axios.js
@@ -1,31 +1,25 @@
 import axios from "axios";
 
 let api = axios.create({
-    baseURL : 'https://advanced-crud-be.onrender.com'
-})
+    baseURL: "https://advanced-crud-be.onrender.com"
+});
 
-//get
-
- export const fetchData = async () => {
-  return await api.get('/Students')
+// get
+export const fetchData = async () => {
+  return await api.get('/students');
 }
-
-
 
 // post
-
-export const postmethood = async (stud)=>{
-    return await api.post('/Students',stud)
+export const postmethood = async (stud) => {
+  return await api.post('/students', stud);
 }
 
+// delete
+export const deletemethood = async (id) => {
+  return await api.delete(`/students/${id}`);
+}
 
-// delete 
- export const deletemethood = async(id)=>{
-    return await api.delete(`/Students/${id}`)
- }
-
-//  edit data
-
-export const editmethod = async (id,data)=>{
-    return await api.put(`Students/${id}`,data)
+// edit
+export const editmethod = async (id, data) => {
+  return await api.put(`/students/${id}`, data);
 }
